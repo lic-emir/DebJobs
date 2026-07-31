4cc8607f22e4b9029cc9f6511985d741392fa9f2
Emir <emir.green.10@gmail.com>
2026-07-31 11:15:58 -0300

    feat(vacante): Generate a new template, controller, and route so that the MÁS INFO button on the home template displays the details of each job.

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -15,3 +15,14 @@ exports.agregarVacante = async (req, res) => {
 
   res.redirect(`/vacantes/${nuevaVacante.url}`)
 }
+exports.mostrarVacante = async (req, res, next) => {
+  const vacante = await Vacante.findOne({url: req.params.url}).lean();
+
+  if(!vacante) return next();
+
+  res.render('vacante', {
+    vacante,
+    nombrePagina:vacante.titulo,
+    barra:true
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ module.exports = () => {
 
   //crear vacantes
   router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante);
-  router.post('/vacantes/nueva', vacantesController.agregarVacante)
+  router.post('/vacantes/nueva', vacantesController.agregarVacante);
+  router.get('/vacantes/:url', vacantesController.mostrarVacante);
   return router;
 }
