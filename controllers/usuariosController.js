d5d732f7b5bdd407c0c6fc310e2f3630575dc84d
Emir <emir.green.10@gmail.com>
2026-08-03 11:46:41 -0300

    feat: store the users in the database

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
+const Usuarios = mongoose.model('Usuarios');
+
 exports.formCrearCuenta = (req, res) => {
   res.render('crear-cuenta', {
     nombrePagina: 'Crea tu cuenta en DevJobs',
     tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta'
   });
+}
+exports.crearUsuario = async (req, res, next) => {
+  const usuario = new Usuarios(req.body);
+
+  try {
+    const nuevoUsuario = await usuario.save();
+    res.redirect('/iniciar-sesion')
+  } catch (error) {
+    return next(error);
+  }
 }
