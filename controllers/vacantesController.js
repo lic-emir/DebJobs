197a37b92a7e72ee8365190b6d843cb244969005
Emir <emir.green.10@gmail.com>
2026-08-14 21:04:49 -0300

    feat(vacante): display recruiter information in the vacante template

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -21,8 +21,8 @@ exports.agregarVacante = async (req, res) => {
   res.redirect(`/vacantes/${nuevaVacante.url}`)
 }
 exports.mostrarVacante = async (req, res, next) => {
-  const vacante = await Vacante.findOne({url: req.params.url}).lean();
-
+  const vacante = await Vacante.findOne({url: req.params.url}).populate('autor', 'nombre email imagen').lean();
+  
   if(!vacante) return next();
 
   res.render('vacante', {
